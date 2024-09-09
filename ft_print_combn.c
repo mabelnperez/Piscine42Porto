cb732242f5a7ef2b9035cd28a789ec4be4cd9d75
mabelnperez <mabel.perez98@gmail.com>
2024-09-09 12:43:34 +0100

    Add files via upload

--- ft_print_combn.c
+++ ft_print_combn.c
@@ -7,26 +7,23 @@ void	ft_putchar(char c)
 
 void	ft_print_number(int num)
 {
-	if (num > 9)
+	if (num > 0)
 	{
 		ft_putchar((num / 10) + '0'); // Imprime o primeiro dígito
+		ft_putchar((num % 10) + '0'); // Imprime o segundo dígito
 	}
-	ft_putchar((num % 10) + '0'); // Imprime o segundo dígito
 }
 
 void	ft_print_combn(int n)
 {
 	int i;
-	int max;
-
+	
 	i = 0;
-	max = 1;
-	while (n--)
-		max *= 10;
-	while (i < max)
+	
+	while (i < n)
 	{
 		ft_print_number(i);
-		if (i < max - 1)
+		if (i < n - 1)
 		{
 			ft_putchar(',');
 			ft_putchar(' ');
