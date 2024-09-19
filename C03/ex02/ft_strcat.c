21cf99b35aff711ed46650f347794e7cfcd3d835
mabelnperez <mabel.perez98@gmail.com>
2024-09-19 11:42:52 +0100

    Add files via upload

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (dest[j] != '\0')
+		j++;
+	while (src[i] != '\0')
+	{
+		dest[j + i] = src[i];
+		i++;
+	}
+	dest[j + i] = '\0';
+	return (dest);
+}
+
+int	main()
+{
+	char	dest[] = "abc123";
+	char	src[] = "def456";
+
+	printf("%s", ft_strcat(dest, src));
+}
